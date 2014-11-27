6421527a416f81cca761b8ef895b38dfd885ee3d
toanht15 <toanht15@gmail.com>
2014-11-27 17:46:15 +0700

    Revert "sign_up"
    This reverts commit 0b1fa167b48b25c64e12cfa8b505f98735a7a0b2.

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-	def full_title(page_title = '')
-		base_title = "Ruby on Rails Sample Apps"
-		if page_title.empty?
-			base_title
-		else
-			"#{page_title} | #{base_title}"
-		end
-	end
+	# Returns the full title on a per-page basis.       # Documentation comment
+  def full_title(page_title = '')                     # Method def, optional arg
+    base_title = "Website Layout Example"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
+    else
+      "#{page_title} | #{base_title}"                 # String interpolation
+    end
+  end
 end
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
+  
+ 
 
-  get 'users/new'
+  root 'static_pages#home'
 
-  root 'pages#home'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'login' => 'static_pages#login'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
-  get 'about' => 'pages#about'
-
-  get 'home' => 'pages#home'
-
-  get 'contact' => 'pages#contact'
-
-  get 'help' => 'pages#help'
-
-  get 'signup' => 'users#new'
-
-  resources :users
-
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
