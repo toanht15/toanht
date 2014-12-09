f21a102d75afb5147130f3daa5b1c26f0360c1b9
toanht15 <toanht15@gmail.com>
2014-12-09 22:18:13 +0700

    finish
    udate,delete,index....

--- Sign_up/db/seeds.rb
+++ Sign_up/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Le Hien",
+	email: "hienhyn@gmail.com",
+	password: "Hien@123",
+	password_confirmation: "Hien@123",
+	admin: true
+	)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@student.hust.edu.vn"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
